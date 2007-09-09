289b249ea3df4a1b91af0b3a45e859d505a38477
duane.johnson@gmail.com <duane.johnson@gmail.com@47880103-7c1f-0410-8e14-d6dd2138f79d>
2007-09-09 04:00:17 +0000

    Added warning message to merb_active_record gem: should be loaded before the app gets loaded
    git-svn-id: http://svn.devjavu.com/merb/plugins@580 47880103-7c1f-0410-8e14-d6dd2138f79d

--- merb_active_record/lib/merb_active_record.rb
+++ merb_active_record/lib/merb_active_record.rb
@@ -1,5 +1,8 @@
 # make sure we're running inside Merb
 if defined?(Merb::Plugins)
+  if Merb::Server.app_loaded?
+    puts "Warning: The merb_active_record gem should be loaded before the application"
+  end
   require 'active_record'
   require "merb_active_record/initialize"
   require "merb_active_record/session"
