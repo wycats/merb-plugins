2330cef3bebaff70fcd3611de486a2451d8aa1be
duane.johnson@gmail.com <duane.johnson@gmail.com@47880103-7c1f-0410-8e14-d6dd2138f79d>
2007-09-11 03:45:21 +0000

    Conform merb_sequel gem to new ORM registration API
    git-svn-id: http://svn.devjavu.com/merb/plugins@613 47880103-7c1f-0410-8e14-d6dd2138f79d

--- merb_sequel/lib/merb_sequel.rb
+++ merb_sequel/lib/merb_sequel.rb
@@ -1,14 +1,11 @@
 # make sure we're running inside Merb
 if defined?(Merb::Plugins)
   if Merb::Server.app_loaded?
-    puts "Warning: The merb_sequel gem should be loaded before the application"
+    puts "Warning: The merb_sequel gem must be loaded before the application"
+  else
+    require "merb/orms/sequel/connection"
+    Merb::Orms::Sequel.connect
+    Merb::Orms::Sequel.register_session_type
   end
-  require 'base64'
-  require "merb_sequel/initialize"
-  require "merb_sequel/session"
-  Merb::Controller.class_eval do
-    include ::Merb::SequelSessionMixin
-    puts "Using Sequel database sessions"
-  end
-  Merb::Plugins.add_rakefiles "merb_sequel/merbtasks"
+  # Merb::Plugins.add_rakefiles "merb_sequel/merbtasks"
 end
--- merb_sequel/lib/merb_sequel/merbtasks.rb
+++
@@ -1,6 +0,0 @@
-namespace :merb_sequel do
-  desc "Do something for merb_sequel"
-  task :default do
-    puts "merb_sequel doesn't do anything"
-  end
-end
