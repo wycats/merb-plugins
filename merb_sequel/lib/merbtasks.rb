3cf3d4d83520db681d78b0487f5490ca27e1c9ec
ez@brainspl.at <ez@brainspl.at@47880103-7c1f-0410-8e14-d6dd2138f79d>
2007-09-21 01:09:29 +0000

    add clear sessions to merb_sequel, closes #199
    git-svn-id: http://svn.devjavu.com/merb/plugins@664 47880103-7c1f-0410-8e14-d6dd2138f79d

--- merb_sequel/lib/merbtasks.rb
+++ merb_sequel/lib/merbtasks.rb
@@ -5,4 +5,13 @@ namespace :sequel do
       Sequel::Migrator.apply(Merb::Orms::Sequel.connect, "schema/migrations", ENV["VERSION"] ? ENV["VERSION"].to_i : nil)
     end
   end
+  
+  namespace :sessions do
+    desc "Clears sessions"
+    task :clear => :merb_env do
+      table_name = (Merb::Plugins.config[:sequel][:session_table_name] || "sessions")
+      
+      Merb::Orms::Sequel.connect.execute("DELETE FROM #{table_name}")
+    end
+  end
 end
