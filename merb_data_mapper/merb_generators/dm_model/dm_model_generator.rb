85e4dcb0055d820f7eed7bb155031d45bfb5a680
has.sox@gmail.com <has.sox@gmail.com@47880103-7c1f-0410-8e14-d6dd2138f79d>
2007-10-10 09:07:13 +0000

    Added model generator to the data mapper plugin
    git-svn-id: http://svn.devjavu.com/merb/plugins@738 47880103-7c1f-0410-8e14-d6dd2138f79d

---
+++ merb_data_mapper/merb_generators/dm_model/dm_model_generator.rb
@@ -0,0 +1,17 @@
+require 'merb/generators/merb_generator_helpers'
+
+class DmModelGenerator < Merb::GeneratorHelpers::ModelGeneratorBase
+  
+  def initialize( *args )
+    super( *args )
+    options[:skip_migration] = true
+    @model_template_name = "new_model_template.erb"
+    # @migration_generator_name = "dm_migration"
+    @model_test_generator_name = "merb_model_test"
+  end
+  
+  def self.superclass
+    RubiGen::Base
+  end
+    
+end
