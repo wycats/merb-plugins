531c3e088bd89714924de353f946602d9fdcbc7f
wycats@gmail.com <wycats@gmail.com@47880103-7c1f-0410-8e14-d6dd2138f79d>
2007-11-07 07:55:51 +0000

    Some tweaks to improve form interoperability between AR and DM
    git-svn-id: http://svn.devjavu.com/merb/plugins@888 47880103-7c1f-0410-8e14-d6dd2138f79d

--- merb_helpers/lib/form_helpers.rb
+++ merb_helpers/lib/form_helpers.rb
@@ -3,14 +3,14 @@ module Merb
     module Form
     
       def error_messages_for(obj, error_li = nil, html_class='submittal_failed')
-        return "" unless obj.errors
-        header_message = block_given? ? yield(obj.errors) : "<h2>Form submittal failed because of #{obj.errors.size} problems</h2>"
+        return "" if obj.errors.empty?
+        header_message = block_given? ? yield(obj.errors) : "<h2>Form submittal failed because of #{obj.errors.size} #{obj.errors.size == 1 ? 'problem' : 'problems'}</h2>"
         ret = %Q{
           <div class='#{html_class}'>
             #{header_message}
             <ul>
         }
-        obj.errors.each {|err| ret << (error_li ? error_li.call(err) : "<li>#{err[0]} #{err[1]}</li>") }
+        obj.errors.each {|err| ret << (error_li ? error_li.call(err) : "<li>#{err.join(" ")}</li>") }
         ret << %Q{
             </ul>
           </div>
