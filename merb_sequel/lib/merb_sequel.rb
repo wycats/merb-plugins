b22efc1bfaa805b80094186ce1f7d4c4cadfb539
wayneeseguin@gmail.com <wayneeseguin@gmail.com@47880103-7c1f-0410-8e14-d6dd2138f79d>
2008-02-01 18:47:08 +0000

    Temporarily removed session registering.
    git-svn-id: http://svn.devjavu.com/merb/plugins@1342 47880103-7c1f-0410-8e14-d6dd2138f79d

--- merb_sequel/lib/merb_sequel.rb
+++ merb_sequel/lib/merb_sequel.rb
@@ -1,6 +1,6 @@
 if defined?(Merb::Plugins)
   require "merb/orms/sequel/connection"
   Merb::Orms::Sequel.connect
-  Merb::Orms::Sequel.register_session_type
+  # Merb::Orms::Sequel.register_session_type
   Merb::Plugins.add_rakefiles "merbtasks"
 end
