6a1c1497e76f7c47a37a95ea74963a2d9734681c
Tim Connor <timocratic@gmail.com>
2008-02-18 09:04:52 -0800

    another tweak to merbtasks for merb_datamapper
    It may not be the long term solution you want, but depending on :merb_init is good enough for these tasks (works on blerb).
    
    Signed-off-by: Ezra Zygmuntowicz <ez@engineyard.com>

--- merb_datamapper/lib/merb_datamapper/merbtasks.rb
+++ merb_datamapper/lib/merb_datamapper/merbtasks.rb
@@ -3,21 +3,21 @@ require 'fileutils'
 namespace :dm do
   namespace :db do
     desc "Perform automigration"
-    task :automigrate => :merb_env do
+    task :automigrate => :merb_init do
       DataMapper::Persistence.auto_migrate!
     end
   end
   
   namespace :sessions do
     desc "Creates session migration"
-    task :create => :merb_env do
+    task :create => :merb_init do
       dest = File.join(Merb.root, "schema", "migrations","001_add_sessions_table.rb")
       source = File.join(File.dirname(__FILE__), "merb", "session","001_add_sessions_table.rb")
       #FileUtils.cp source, dest unless File.exists?(dest)
     end
     
     desc "Clears sessions"
-    task :clear => :merb_env do
+    task :clear => :merb_init do
       table_name = (Merb::Plugins.config[:datamapper][:session_table_name] || "sessions")
       #Merb::Orms::DataMapper.connect.execute("DELETE FROM #{table_name}")
     end
