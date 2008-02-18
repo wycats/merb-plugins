aa8a74fcac52766eb8ae442a8f69bd94406b7322
Paul Barry <mail@paulbarry.com>
2008-02-18 15:19:39 -0500

    Added to_param method for data_mapper
    Signed-off-by: Michael D. Ivey <ivey@gweezlebur.com>

--- merb_datamapper/lib/merb_datamapper.rb
+++ merb_datamapper/lib/merb_datamapper.rb
@@ -1,6 +1,7 @@
 if defined?(Merb::Plugins)
   Merb::BootLoader.before_app_loads do
     require File.join(File.dirname(__FILE__) / "merb" / "orms" / "data_mapper" / "connection")
+    require File.join(File.dirname(__FILE__) / "merb" / "orms" / "data_mapper" / "base")
     Merb::Orms::DataMapper.connect
     Merb::Orms::DataMapper.register_session_type
   end
