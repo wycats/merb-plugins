a15f43ae003cc68c671ab325586fa026f5673373
Michael S. Klishin <michael@novemberain.com>
2008-05-11 19:59:34 +0300

    Use simple before_app_loads hook instead of bootloader subclass.

--- merb_activerecord/lib/merb_activerecord.rb
+++ merb_activerecord/lib/merb_activerecord.rb
@@ -1,17 +1,10 @@
-if defined?(Merb::Plugins)  
+if defined?(Merb::Plugins)
   dependency "activerecord"
   require File.join(File.dirname(__FILE__) / "merb" / "orms" / "active_record" / "connection")
   Merb::Plugins.add_rakefiles(File.join(File.dirname(__FILE__) / "active_record" / "merbtasks"))
-  
-  class Merb::Orms::ActiveRecord::Connect < Merb::BootLoader
 
-    after BeforeAppLoads
-
-    def self.run
-      Merb::Orms::ActiveRecord.connect
-      Merb::Orms::ActiveRecord.register_session_type
-    end
-
+  Merb::BootLoader.before_app_loads do
+    Merb::Orms::ActiveRecord.connect
+    Merb::Orms::ActiveRecord.register_session_type
   end
-
 end
--- merb_sequel/lib/merb_sequel.rb
+++ merb_sequel/lib/merb_sequel.rb
@@ -2,16 +2,9 @@ if defined?(Merb::Plugins)
   Merb::Plugins.config[:merb_sequel] = {}
   require File.join(File.dirname(__FILE__) / "merb" / "orms" / "sequel" / "connection")
   Merb::Plugins.add_rakefiles "merb_sequel" / "merbtasks"
-  
-  class Merb::Orms::Sequel::Connect < Merb::BootLoader
 
-    after BeforeAppLoads
-
-    def self.run
-      Merb::Orms::Sequel.connect
-      Merb::Orms::Sequel.register_session_type
-    end
-
+  Merb::BootLoader.before_app_loads do
+    Merb::Orms::Sequel.connect
+    Merb::Orms::Sequel.register_session_type
   end
-  
 end
