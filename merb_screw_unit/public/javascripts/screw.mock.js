c24fb2de1305c9c16d1f99fa3b640dca00591387
Yehuda Katz <wycats@yehuda-katzs-macbook-pro.local>
2008-07-07 01:57:18 -0700

    Support status codes in mock Ajax

--- merb_screw_unit/public/javascripts/screw.mock.js
+++ merb_screw_unit/public/javascripts/screw.mock.js
@@ -8,10 +8,59 @@ Screw.Mock = {
   }
 };
 Screw.XHR = function() {};
-Screw.XHR.returns = function(str, type) {
+Screw.XHR.returns = function(str, type, status) {
+  codes = {
+    100: "Continue", 
+    101: "Switching Protocols", 
+    200: "OK", 
+    201: "Created", 
+    202: "Accepted", 
+    203: "Non-Authoritative Information",
+    204: "No Content", 
+    205: "Reset Content", 
+    206: "Partial Content",
+    300: "Multiple Choices",
+    301: "Moved Permanently",
+    302: "Found",
+    303: "See Other",
+    304: "Not Modified",
+    305: "Use Proxy",
+    307: "Temporary Redirect",
+    400: "Bad Request",
+    401: "Unauthorized",
+    402: "Payment Required",
+    403: "Forbidden",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    406: "Not Acceptable",
+    407: "Proxy Authentication Required",
+    408: "Request Timeout",
+    409: "Conflict",
+    410: "Gone",
+    411: "Length Required",
+    412: "Precondition Failed",
+    413: "Request Entity Too Large",
+    414: "Request-URI Too Long",
+    415: "Unsupported Media Type",
+    416: "Requested Range Not Satisfiable",
+    417: "Expectation Failed",
+    422: "Unprocessable Entity",
+    500: "Internal Server Error",
+    501: "Not Implemented",
+    502: "Bad Gateway",
+    503: "Service Unavailable",
+    504: "Gateway Timeout",
+    505: "HTTP Version Not Supported"
+  }  ;
   Screw.XHR.prototype.responseText = str;
-  Screw.XHR.prototype.headers = {"content-type": "application/json"};
+  Screw.XHR.prototype.status = status || 200;
+  Screw.XHR.prototype.statusText = codes[status || 200];
+  Screw.XHR.prototype.headers = Screw.XHR.prototype.headers || {};
+  Screw.XHR.prototype.headers["content-type"] = type;
   if(type == "xml") Screw.XHR.prototype.responseXML = $(str).get();
+};
+Screw.XHR.headers = function(headers) {
+  Screw.XHR.prototype.headers = headers;
 };
 Screw.XHR.prototype = {
   abort: function() {},
@@ -24,8 +73,6 @@ Screw.XHR.prototype = {
   },
   send: function(content) {
     this.readyState = 4;
-    this.status = 200;
-    this.statusText = "OK";
   },
   setRequestHeader: function(label, value) {}
 };
