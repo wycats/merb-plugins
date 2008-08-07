83c11a993209d484cc0ae85410181e0a401c71be
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-08-07 22:34:47 +0200

    added index to session migration for activerecord

--- merb_activerecord/lib/generators/templates/session_migration/schema/migrations/%version%_sessions.rb
+++ merb_activerecord/lib/generators/templates/session_migration/schema/migrations/%version%_sessions.rb
@@ -1,12 +1,14 @@
 class DatabaseSessions < ActiveRecord::Migration
   def self.up
     create_table :sessions do |t|
-      t.column :session_id, :text
+      t.column :session_id, :string
       t.column :data,       :text
     end
+    add_index :sessions, :session_id
   end
 
   def self.down
+    remove_index :sessions, :session_id
     drop_table :sessions
   end
 end
