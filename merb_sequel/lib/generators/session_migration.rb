60ecb7badabf44ac2eede98ff2e5cc1fb6d4d0cf
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-09-09 20:27:48 +0200

    templater 0.2 syntax for merb_sequel

--- merb_sequel/lib/generators/session_migration.rb
+++ merb_sequel/lib/generators/session_migration.rb
@@ -1,4 +1,4 @@
-Merb::Generators::SessionMigrationGenerator.template :session_migration_sequel, :orm => :sequel do
-  source(File.dirname(__FILE__), 'templates/session_migration/schema/migrations/%version%_sessions.rb')
-  destination("schema/migrations/#{version}_sessions.rb")
+Merb::Generators::SessionMigrationGenerator.template :session_migration_sequel, :orm => :sequel do |t|
+  t.source = File.dirname(__FILE__) / 'templates/session_migration/schema/migrations/%version%_sessions.rb'
+  t.destination = "schema/migrations/#{version}_sessions.rb"
 end
