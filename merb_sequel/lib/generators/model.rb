579436e3fa6b9367dcc723851ffa88e208ab1cfe
Jaroslaw Zabiello <hipertracker@gmail.com>
2008-09-13 09:09:37 +0100

    Fixed broken Sequel generator
    Signed-off-by: Michael S. Klishin <michael@novemberain.com>

--- merb_sequel/lib/generators/model.rb
+++ merb_sequel/lib/generators/model.rb
@@ -1,4 +1,4 @@
-Merb::Generators::ModelGenerator.template :model_sequel, :orm => :sequel do |y|
+Merb::Generators::ModelGenerator.template :model_sequel, :orm => :sequel do |t|
   t.source = File.dirname(__FILE__) / "templates/model/app/models/%file_name%.rb"
   t.destination = "app/models" / base_path / "#{file_name}.rb"
 end
